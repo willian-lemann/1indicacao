bfbbd25e48bcdd1451dd141364010b4f32478095
Willian Lemann <willianleman@hotmail.com>
2023-04-17 11:32:12 -0300

    feat: login page localization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,39 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 import { api } from "@/utils/api";
+import { useRouter } from "next/router";
+
+const publicPages = ["/login"];
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const { pathname } = useRouter();
+
+  const isPublicPage = publicPages.includes(pathname);
+
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      {...pageProps}
+      localization={{
+        socialButtonsBlockButton: "Continue com google",
+        formFieldLabel__emailAddress: "Email",
+        dividerText: "ou",
+        signIn: {
+          start: {
+            title: "Faça login",
+            subtitle: "para entrar no lokadin",
+            actionText: "Não tem uma conta?",
+            actionLink: "Cadastrar-se",
+          },
+        },
+      }}
+    >
       <Component {...pageProps} />
     </ClerkProvider>
   );
